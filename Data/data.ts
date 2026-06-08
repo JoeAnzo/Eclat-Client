e6894ccf8f25f447a444c0f29121fad028341704
JoeAnzo <joelanzovule@gmail.com>
2026-06-08 14:28:37 +0300

    Remove howItWorks export from data.ts

--- Data/data.ts
+++ Data/data.ts
@@ -1,11 +1,1 @@
-import {ShoppingBag} from "lucide-react"
 
-
-export const howItWorks = [
-    {
-        number:01,
-        icon:,
-        heading:"Browse & Add",
-        text:"Tap through the shop. Add anything you like to your cart",
-    }
-]
