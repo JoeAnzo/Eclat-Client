d72e344eb8887a20eaf70bed3a04a8e181784c06
JoeAnzo <joelanzovule@gmail.com>
2026-06-18 13:26:07 -0700

    Something changes

--- app/page.tsx
+++ app/page.tsx
@@ -39,13 +39,13 @@ export default function Home() {
         <Couresel/>
         <div className="absolute left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50%] space-y-5  z-5">
           <Animation variant="slideRight">
-            <h2 className="sm:text-4xl text-5xl font-semibold font-playfair text-(--primary-color) my-4">Your Essence, Your Signature</h2>
+            <h2 className="sm:text-4xl text-5xl font-semibold font-playfair text-(--primary-color) my-5">Your Essence, Your Signature</h2>
           </Animation>
           <Animation variant="slideUp">
-            <p className="text-white text-md sm:text-2xl font-inter prose mb-4">Find the scent that speaks your language and defines your everyday moments </p>
+            <p className="text-white text-md sm:text-2xl font-inter prose mb-5">Find the scent that speaks your language and defines your everyday moments </p>
           </Animation>
           <Animation variant="slideUp">
-            <div className="flex flex-col sm:flex-row gap-2 justify-start items-left">
+            <div className="flex flex-col sm:flex-row gap-4 justify-start items-left">
               <Button size="lg" variant="default" className="bg-(--primary-color) h-12 px-8 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
                 Shop now
                 <ArrowRight/>
@@ -67,7 +67,7 @@ export default function Home() {
           <div className="h-1 bg-(--secondary-color) rounded-md w-20"></div>
         </div>
         <Animation variant="slideUp">
-          <h2 className="text-center font-playfair px-2 text-(--text) my-2 text-3xl">Discover our most-coveted bottles and find your match.</h2>
+          <h2 className="text-center font-playfair px-2 text-(--text) my-5 text-3xl">Discover our most-coveted bottles and find your match.</h2>
         </Animation>
         <BestSellers/>
       </section>
@@ -79,7 +79,7 @@ export default function Home() {
           <div className="h-1 mb-4 bg-(--secondary-color) rounded-md w-20"></div>
         </div>
         <Animation variant="slideUp">
-          <h2 className="px-2 font-playfair text-(--text) text-3xl text-center my-4">Three taps, one delivery</h2>
+          <h2 className="px-2 font-playfair text-(--text) text-3xl text-center my-5">Three taps, one delivery</h2>
           <div className="space-y-4 px-4">
               {
                 howItWorks.map((item,index) => {
@@ -95,17 +95,6 @@ export default function Home() {
       <section>
         <div className="flex flex-col justify-center items-center">
           <Animation variant="slideRight">
-            <h2 className="text-(--text) text-center my-4">Testimonials</h2>
-          </Animation>
-          <div className="h-1 mb-4 bg-(--secondary-color) rounded-md w-20"></div>
-        </div>
-        <Animation variant="slideUp">
-          <h2 className="px-2 font-playfair text-(--text) text-3xl text-center my-4">What Our Customers have to say</h2>
-        </Animation>
-      </section>
-      <section>
-        <div className="flex flex-col justify-center items-center">
-          <Animation variant="slideRight">
             <h2 className="text-(--text) text-center my-4">Brands</h2>
           </Animation>
           <div className="h-1 mb-4 bg-(--secondary-color) rounded-md w-20"></div>
@@ -115,6 +104,17 @@ export default function Home() {
         </Animation>
         <Animation variant="slideUp">
           <LogoCarousel/>
+        </Animation>
+      </section>
+      <section>
+        <div className="flex flex-col justify-center items-center">
+          <Animation variant="slideRight">
+            <h2 className="text-(--text) text-center my-4">Testimonials</h2>
+          </Animation>
+          <div className="h-1 mb-4 bg-(--secondary-color) rounded-md w-20"></div>
+        </div>
+        <Animation variant="slideUp">
+          <h2 className="px-2 font-playfair text-(--text) text-3xl text-center my-4">What Our Customers have to say</h2>
         </Animation>
       </section>
     </main>
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -13,6 +13,13 @@ function ProductCard({name,url,price}:Product) {
           <Image src={url} alt={name} fill sizes="(max-width:768px) 100vw, 320px" className="max-w-full border-b border-(--secondary-color) h-auto object-cover"/>
         </div>
         <Animation variant="slideUp">
+          <div className="relative bg-(--background)">
+            <div className="p-4 absolute top-0 bottom-0 left-0 right-0 bg-white/30 backdrop-blur-xl backdrop-filter">
+            <p className="py-2 font-bold font-playfair">{name}</p>
+            <p className="pb-2 font-space">{price}</p>
+            <Button variant="default" className="rounded-none bg-(--primary-color) h-12 px-8 font-semibold  flex gap-2">ADD <ShoppingBag color="white"/></Button>
+          </div>
+          </div>
           <div className="p-4">
             <p className="py-2 font-bold font-playfair">{name}</p>
             <p className="pb-2 font-space">{price}</p>
