510a6a63e859ab71f4dd31e5d076c81829f81aa1
JoeAnzo <joelanzovule@gmail.com>
2026-06-18 13:15:46 -0700

    some changes made

--- components/Header.tsx
+++ components/Header.tsx
@@ -32,13 +32,12 @@ const Header = () => {
   return (
     <header className={`fixed z-10 top-0 left-0 right-0 flex justify-between items-center ${hasScrolled ? 'bg-white/30 backdrop-blur-xl backdrop-filter':'bg-transparent'} py-4 px-2 border border-(--secondary-color)`}>
         <h1 className="text-xl font-bold font-playfair text-[#4A2C6D]">Eclat Essence</h1>
-        
-          <nav className={`sm:space-x-2 py-2 pl-2 right-0 space-y-2 ${ openMenu ? 'flex bg-(--secondary-color) border border-(--secondary-color) sm:bg-transparent':'hidden sm:flex'} flex-col sm:flex-row font-sans absolute sm:relative top-full left-0 z-500`}>
-              <Link className="border-b text-white sm:text-(--text) border-gray-300 py-2 sm:border-none hover:text-(--secondary-color)" href="/">Home</Link>
-              <Link className="border-b text-white sm:text-(--text) border-gray-300 py-2 sm:border-none hover:text-(--secondary-color)" href="/shop">Shop</Link>
-              <Link className="border-b text-white sm:text-(--text) border-gray-300 py-2 sm:border-none hover:text-(--secondary-color)" href="/how-it-works">How it works</Link>
-              <Link className="border-b text-white sm:text-(--text) border-gray-300 py-2 sm:border-none hover:text-(--secondary-color)" href="/about-us">About</Link>
-              <Link className="text-white sm:text-(--text) py-2 sm:border-none hover:text-(--secondary-color)" href="/contact-us">Contact</Link>
+          <nav className={`sm:space-x-2 py-2 pl-2 right-0 space-y-2 ${ openMenu ? 'flex bg-(--background) border border-(--secondary-color) sm:bg-transparent':'hidden sm:flex'} flex-col sm:flex-row font-sans absolute sm:relative top-full left-0 z-500`}>
+              <Link className="border-b text-(--text) border-gray-300 py-2 sm:border-none hover:text-(--secondary-color)" href="/">Home</Link>
+              <Link className="border-b text-(--text) border-gray-300 py-2 sm:border-none hover:text-(--secondary-color)" href="/shop">Shop</Link>
+              <Link className="border-b text-(--text) border-gray-300 py-2 sm:border-none hover:text-(--secondary-color)" href="/how-it-works">How it works</Link>
+              <Link className="border-b text-(--text) border-gray-300 py-2 sm:border-none hover:text-(--secondary-color)" href="/about-us">About</Link>
+              <Link className="text-white text-(--text) py-2 sm:border-none hover:text-(--secondary-color)" href="/contact-us">Contact</Link>
           </nav>
         <div className='flex gap-2'>
           <div className="flex gap-2">
