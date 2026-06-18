7377b673a32208a0ab357b386d412f752d75ef7c
JoeAnzo <joelanzovule@gmail.com>
2026-06-18 08:53:40 -0700

    Website header color update

--- components/HowItWorksCard.tsx
+++ components/HowItWorksCard.tsx
@@ -12,15 +12,16 @@ export default function HowItWorksCard({step,title,icon}:howItWorksProps){
         <Animation variant="slideUp">
             <div className="border border-(--secondary-color) rounded-md flex flex-col items-center bg-white/30 backdrop-blur-xl backdrop-filter">
                 <Animation variant="slideUp">
-                    <div className="h-10 w-10 bg-(--primary-color) flex items-center justify-center rounded-full">
+                    <div className="h-20 w-20 my-2 bg-(--primary-color) flex items-center justify-center rounded-full">
                         {icon}
                     </div>
                 </Animation>
                 <Animation variant="slideUp">
-                    <h2 className="font-playfair my-2 text-center text-(--primary-color) flex">{step}{title}</h2>
+                    <h2 className="font-playfair text-2xl my-2 text-center text-(--primary-color) flex">{step}</h2>
                 </Animation>
-                
-                
+                <Animation variant="slideUp">
+                    <h2 className="font-playfair text-2xl my-2 text-center text-(--primary-color) flex">{title}</h2>
+                </Animation>            
             </div>
         </Animation>
 
