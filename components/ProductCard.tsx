07e37a6222164731822c4639355482d1e292c2bf
JoeAnzo <joelanzovule@gmail.com>
2026-06-19 04:21:44 -0700

    profile pics update

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -17,16 +17,8 @@ function ProductCard({name,url,price}:Product) {
             <div className="p-4 absolute top-0 bottom-0 left-0 right-0 bg-white/30 backdrop-blur-xl backdrop-filter">
             <p className="py-2 font-bold font-playfair">{name}</p>
             <p className="pb-2 font-space">{price}</p>
-            <Button variant="default" className="rounded-none bg-(--primary-color) h-12 px-8 font-semibold  flex gap-2">ADD <ShoppingBag color="white"/></Button>
-          </div>
-          </div>
-          <div className="p-4">
-            <p className="py-2 font-bold font-playfair">{name}</p>
-            <p className="pb-2 font-space">{price}</p>
-            <Button variant="default" className="rounded-none bg-(--primary-color) h-12 px-8 font-semibold  flex gap-2">
-              ADD TO CART 
-              <ShoppingBag color="white"/>
-            </Button>
+            <Button variant="default" className="rounded-none bg-(--primary-color) h-12 px-8 font-semibold  flex gap-2">ADD TO CART <ShoppingBag color="white"/></Button>
+            </div>
           </div>
         </Animation>
       </div>
