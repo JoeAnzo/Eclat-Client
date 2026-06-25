df69db533f78f7581df58b9245b9c88b5953351f
JoeAnzo <joelanzovule@gmail.com>
2026-06-25 06:43:47 -0700

    work cart on

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -8,7 +8,7 @@ export default function Cart() {
     <section className="min-h-screen">
       {
         cart.length === 0 ? 
-        <div className="h-full flex flex-col items-center justify-center">
+        <div className="h-full flex flex-col items-center justify-center bg-red">
             <ShoppingBag/>
             <h2>Your Cart is empty</h2>
               <Button size="lg" variant="default" className="bg-(--primary-color) h-12 px-8 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
