4e301f6485006c0520b139a12fb1cf09324da51a
JoeAnzo <joelanzovule@gmail.com>
2026-07-22 10:13:45 -0700

    hello

--- app/shop/[slug]/page.tsx
+++ app/shop/[slug]/page.tsx
@@ -1,24 +1,37 @@
 import { fragranceService } from "@/app/services/fragrance.service"
 import Image from "next/image"
+import { notFound } from "next/navigation"
+
 interface pageProps {
-    params:Promise<{slug:string}>
+    params: Promise<{ slug: string }>
 }
 
-export default async function fragranceDetailsPage({params}:pageProps){
-    const {slug} = await params
+export default async function fragranceDetailsPage({ params }: pageProps) {
+    const { slug } = await params
 
     const response = await fragranceService.getFragranceBySlug(slug)
-    const product = response.data
+    const product = response?.data
 
-    console.log(product)
+    if (!product) {
+        notFound() 
+    }
 
-    return(
+    return (
         <section>
             <div>
-                <div>
-                    <Image src={product.thumbnailUrl} alt={product.name} fill sizes="(max-width:768px) 100vw, 320px" className="max-w-full rounded-t-lg h-auto object-cover"/>
+                
+                <div className="relative w-full h-[400px]"> 
+                    <Image 
+                        src={product.thumbnailUrl} 
+                        alt={product.name} 
+                        fill 
+                        sizes="(max-width:768px) 100vw, 320px" 
+                        className="rounded-t-lg object-cover"
+                        priority 
+                    />
                 </div>
                 <div>
+                    <h1>{product.name}</h1>
                     <p>{product.price}</p>
                     <p>{product.description}</p>
                 </div>
