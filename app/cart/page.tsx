da3f48fcc65e3ef3da05ee7e60c4d0bdced6be16
JoeAnzo <joelanzovule@gmail.com>
2026-07-22 06:02:11 -0700

    removed hook

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,24 +3,9 @@ import useCart from "@/hooks/useCart"
 import { ShoppingBag,ArrowRight } from "lucide-react"
 import { Button } from "@/components/ui/button"
 export default function Cart() {
-  const {cart} = useCart()
+
   return (
     <section className="min-h-screen">
-      {
-        cart.length === 0 ? 
-        <div className="h-screen flex flex-col gap-2 items-center justify-center">
-            <ShoppingBag size={40}/>
-            <h2 className="font-bold text-xl font-inter">Your vanity is empty!</h2>
-            <p className="italic px-4">Don't leave your perfume tray lonely. Grab your favourites before they sell out.</p>
-              <Button size="lg" variant="default" className="bg-(--primary-color) h-12 px-8 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
-                Start Shopping
-                <ArrowRight/>
-              </Button>
-        </div>:
-        <div>
-
-        </div>
-      }
     </section>
   )
 }
