8c0b258f12a49c6a67f17f605a26919ffbc426ff
JoeAnzo <joelanzovule@gmail.com>
2026-07-24 11:34:06 -0700

    search icon

--- components/SearchAndFilter.tsx
+++ components/SearchAndFilter.tsx
@@ -1,7 +1,7 @@
 import { Search,FunnelIcon } from "lucide-react"
 export default function SearchAndFilter(){
     return(
-    <div className="flex justify-center gap-2.5">
+    <div className="flex gap-2.5">
       <div className="relative">
         <input type="text" className="bg-[#F5F5F5] max-w-190 h-10 placeholder:font-sans" placeholder="search fragrances,brands&categories"/>
         <Search className="absolute -left-2 top-[50%] translate-y-[-50%]"/>
