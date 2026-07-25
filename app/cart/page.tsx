214d4dffbae70da0ef14b213ef96449ca19c55a2
JoeAnzo <joelanzovule@gmail.com>
2026-07-25 06:00:45 -0700

    quantity buttons

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,13 +1,14 @@
 "use client"
-import { ShoppingBag,ArrowRight } from "lucide-react"
+import { ShoppingBag,ArrowRight,Trash } from "lucide-react"
 import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import {useCart} from "@/hooks/useCart"
 import { CartItem } from "../Interfaces/product.interface"
+import QuantityButtons from "@/components/QuantityButtons"
 
 export default function Cart() {
 
-  const {cart,totalItems,totalPrice} = useCart()
+  const {cart,totalItems,totalPrice,clearCart,removeFromCart,updateQuantity} = useCart()
   return (
     <section className="min-h-screen">
       {
@@ -21,35 +22,35 @@ export default function Cart() {
           </Button>
         </div>
         :
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 t-8">
           <h2 className="text-xl font-semi-bold font-sans">Cart Summary</h2>
-          <p>Total Items:{totalItems}</p>
-          <p>Total Price:{totalPrice}</p>
+          <p className="font-space">Cart: ({totalItems})</p>
+          <p className="font-space">Sub Total:{totalPrice}</p>
           {
             cart.map((item:CartItem) => {
               return <div key={item.id} className="flex gap-2 bg-[#F5F5F5]">
                 <div className="w-[400px] h-[400px]">
-                  <Image src={item.thumbnailUrl} alt={item.name} className="w-full h-full"/>
+                  <Image src={item.thumbnailUrl} alt={item.name} className="max-w-full h-auto"/>
                 </div>
-                <div>
+                <div className="p-2">
                   <h2 className="font-playfair font-semibold text-md">{item.name}</h2>
                   <p className="font-space">UGX {item.price.toLocaleString()}</p>
                   <div className="flex gap-2">
-                    <Button size="lg" variant="default" className="bg-(--primary-color) h-12 px-8 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
-                      +
-                    </Button>
-                    <p className="font-space">QTY:{item.quantity}</p>
-                    <Button size="lg" variant="default" className="bg-(--primary-color) h-12 px-8 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
-                      -
+                    <QuantityButtons item={item} updateQuantity={updateQuantity}/>
+                     <Button onClick={() => removeFromCart(item.id)} size="lg" variant="default" className="bg-transparent px-4 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
+                      remove <Trash/>
                     </Button>
                   </div>
                 </div>
               </div>
             })
           }
-          <div className="sticky bottom-0">
+          <div className="sticky py-1.5 bottom-0 flex flex-col gap-2 items-center justify-center bg-[#F5F5F5]">
               <Button size="lg" variant="default" className="bg-(--primary-color) h-12 px-8 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
-                CheckOut {totalPrice.toLocaleString()}
+                CheckOut (UGX {totalPrice.toLocaleString()})
+              </Button>
+              <Button onClick={() => clearCart()} size="lg" variant="default" className="bg-(--primary-color) h-12 px-8 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
+                Clear Cart
               </Button>
           </div>
         </div>
---
+++ components/QuantityButtons.tsx
@@ -0,0 +1,33 @@
+"use client"
+import {useState} from "react"
+import { Button } from "@/components/ui/button"
+import { CartItem } from "@/app/Interfaces/product.interface"
+
+
+interface QuantityButtonProps {
+    item:CartItem;
+    updateQuantity: (productId:string,quantity:number) => void
+}
+
+export default function QuantityButtons({item,updateQuantity}:QuantityButtonProps){
+    const [quantity,setQuantity] = useState(1)
+    const addQuantity = () => {
+        setQuantity(prev => prev + 1)
+        updateQuantity(item.id,quantity)
+    }
+    const subtractQuantity = () => {
+        setQuantity(prev => prev - 1)
+        updateQuantity(item.id,quantity)
+    }
+    return(
+        <>
+            <Button onClick={addQuantity} size="lg" variant="default" className="bg-(--primary-color) px-4 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
+                +
+            </Button>
+            <p className="font-space">{item.quantity}</p>
+            <Button onClick={subtractQuantity} size="lg" variant="default" className="bg-(--primary-color) px-4 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
+                -
+            </Button>
+        </>
+    )
+}
