024f1ed13c0ce944756830a4e0ae6f2f2b06d5d6
JoeAnzo <joelanzovule@gmail.com>
2026-07-26 05:38:30 -0700

    hello world

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -13,7 +13,7 @@ export default function Cart() {
     <section className="min-h-screen">
       {
         cart.length === 0 ? 
-        <div className="flex flex-col items-center justify-center h-full gap-2">
+        <div className="flex flex-col items-center justify-center h-full gap-2 bg-red-100">
           <ShoppingBag/>
           <h2 className="text-xl font-semibold font-playfair">Cart is empty</h2>
           <Button size="lg" variant="default" className="bg-(--primary-color) h-12 px-8 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
@@ -22,10 +22,10 @@ export default function Cart() {
           </Button>
         </div>
         :
-        <div className="flex flex-col gap-2 t-8">
-          <h2 className="text-xl font-semi-bold font-sans">Cart Summary</h2>
-          <p className="font-space">Cart: ({totalItems})</p>
-          <p className="font-space">Sub Total:{totalPrice}</p>
+        <div className="flex flex-col gap-2 pt-8">
+          <h2 className="text-xl pl-8 font-semi-bold font-sans">Cart Summary</h2>
+          <p className="font-space pl-8">Cart: ({totalItems})</p>
+          <p className="font-space pl-8">Sub Total:{totalPrice}</p>
           {
             cart.map((item:CartItem) => {
               return <div key={item.id} className="flex gap-2 bg-[#F5F5F5]">
@@ -35,19 +35,19 @@ export default function Cart() {
                 <div className="flex flex-col p-2">
                   <h2 className="font-playfair font-semibold text-md">{item.name}</h2>
                   <p className="font-space">UGX {item.price.toLocaleString()}</p>
-                  <div className="flex gap-2">
+                  <div className="flex flex-col gap-2">
                     <QuantityButtons item={item} updateQuantity={updateQuantity}/>
-                    <p onClick={() => removeFromCart(item.id)} className="text-[--primary-color]">Remove item <Trash/></p>
+                    <p onClick={() => removeFromCart(item.id)} className="text-(--primary-color) flex items-center justify-center">Remove item <Trash/></p>
                   </div>
                 </div>
               </div>
             })
           }
-          <div className="sticky py-1.5 bottom-0 flex flex-col gap-2 items-center justify-center bg-[#F5F5F5]">
-              <Button size="lg" variant="default" className="bg-(--primary-color) h-12 px-8 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
+          <div className="sticky py-1.5 bottom-0 flex flex-col gap-2 items-center justify-center bg-white px-8">
+              <Button size="lg" variant="default" className="bg-(--primary-color) h-12 w-full px-8 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
                 CheckOut (UGX {totalPrice.toLocaleString()})
               </Button>
-              <Button onClick={() => clearCart()} size="lg" variant="default" className="bg-(--primary-color) h-12 px-8 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
+              <Button onClick={() => clearCart()} size="lg" variant="default" className="bg-(--primary-color) w-full h-12 px-8 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
                 Clear Cart
               </Button>
           </div>
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,13 +1,16 @@
+'use client'
 import Image from "next/image"
 import { Product } from "@/app/Interfaces/product.interface"
 import { Button } from "./ui/button"
 import { ShoppingBag,HeartIcon } from "lucide-react"
 import Animation from "./Animation"
 import Link from "next/link"
+import { useCart } from "@/hooks/useCart"
 
 type productCard = Omit<Product,'id'>
 
 function ProductCard({name,thumbnailUrl,price,slug,gender}:productCard) {
+  const {addToCart} = useCart()
   type genderFormat = 'Men' | 'Women' | 'Unisex'
   let genderFormat:genderFormat
   if (gender === 'MALE'){
@@ -17,22 +20,26 @@ function ProductCard({name,thumbnailUrl,price,slug,gender}:productCard) {
   } else {
     genderFormat = 'Unisex'
   }
+  
   return (
     <Animation variant="slideUp">
       <Link href={`/shop/${slug}`}>
         <div className="rounded-lg shadow-md max-w-85  relative">
-        <div className="absolute z-100 right-2 top-2 flex items-center justify-center bg-[#F5F5F5] h-8 w-8 rounded-full">
-          <HeartIcon/>
+        <div className="absolute z-100 top-2 left-2 flex items-center justify-center bg-[#F5F5F5] h-8 w-8 rounded-full">
+          <h2 className="font-playfair font-semibold text-md">{genderFormat}</h2>
         </div>
         <div className="relative h-80 w-full overflow-hidden">
           <Image src={thumbnailUrl} alt={name} fill sizes="(max-width:768px) 100vw, 320px" className="max-w-full rounded-t-lg h-auto object-cover"/>
+          <div className="absolute z-100 right-2 bottom-2 flex items-center justify-center bg-[#F5F5F5] h-8 w-8 rounded-full">
+            <HeartIcon/>
+          </div>
         </div>
         <Animation variant="none">
           <div className="flex w-full items-center justify-center">
             <div className="pb-4 rounded-b-lg flex flex-col items-start">
               <p className="py-2 font-bold font-playfair">{name}</p>
               <p className="pb-2 font-space">UGX {price.toLocaleString()}</p>
-              <Button className="rounded-none text-white p-2 bg-(--primary-color)" variant="default">Add to cart</Button>
+              <Button className="rounded-none text-white p-2.5 w-full bg-(--primary-color)" variant="default">Add to cart <ShoppingBag/></Button>
             </div>
           </div>
         </Animation>
