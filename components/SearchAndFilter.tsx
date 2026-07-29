71e724831d005290d4aad4e17817d1a3d9c5fc6f
JoeAnzo <joelanzovule@gmail.com>
2026-07-29 08:41:15 +0300

    Update SearchAndFilter.tsx

--- components/SearchAndFilter.tsx
+++ components/SearchAndFilter.tsx
@@ -3,12 +3,12 @@ import { Button } from "@/components/ui/button"
 
 export default function SearchAndFilter(){
     return(
-    <div className="flex gap-2.5 items-center">
+    <div className="flex flex-col gap-2.5 items-center">
       <div className="flex items-center justify-center px-[20px] gap-2 relative">
         <input type="text" className="bg-[#F5F5F5] w-full max-w-190 h-10 placeholder:font-sans" placeholder="search fragrances,brands&categories"/>
         <Search className="absolute top-[50%] translate-y-[-50%] "/>
       </div>
-      <div className="flex gap-2">
+      <div className="flex gap-2 flex-wrap">
           <Button size="lg" variant="default" className="bg-(--primary-color) h-12 px-8 font-semibold text-lg shadow-lg tracking-wide text-white flex gap-2 rounded-none">
             Brands
             <ChevronDown/>
